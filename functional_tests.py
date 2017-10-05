7ad4d79542eff4293996848e7a9b79732f529c5c
Morgan Krey <morgankrey@Morgans-MacBook-Air.local>
2017-10-04 22:04:28 -0400

    first FT inserted, uses unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,19 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert '' in browser.title
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_can_get_on_homepage(self):
+		self.browser.get('http://localhost:8000')
+
+		self.assertIn('Podcastery', self.browser.title)
+		self.fail('Finish the test!')
+
+if __name__=='__main__':
+	unittest.main()
